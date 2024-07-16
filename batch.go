7209bd0e265798e0e7303e8f4b7ae80a081069e8
qiluo1234 <3160351289@qq.com>
2024-07-16 08:39:11 +0000

    增加了batch.go,以及相应的测试

--- batch.go
+++ batch.go
@@ -7,6 +7,8 @@ import (
 	"sync/atomic"
 )
 
+const nonTransactionSeqNo uint64 = 0
+
 var txnFinKey = []byte("txn-fin")
 
 // WriteBatch 原子批量写数据，保证原子性
@@ -17,7 +19,7 @@ type WriteBatch struct {
 	pendingWrites map[string]*data.LogRecord //暂存用户写入的数据
 }
 
-func (db *DB) NeWriteBatch(opts WriteBatchOptions) *WriteBatch {
+func (db *DB) NewWriteBatch(opts WriteBatchOptions) *WriteBatch {
 	return &WriteBatch{
 		options:       opts,
 		mu:            new(sync.Mutex),
@@ -63,7 +65,7 @@ func (wb *WriteBatch) Delete(key []byte) error {
 	return nil
 }
 
-// Commit 提交事务，将暂存的数据写道数据文件，并更新内存索引
+// Commit 提交事务，将暂存的数据写到数据文件，并更新内存索引
 func (wb *WriteBatch) Commit() error {
 	wb.mu.Lock()
 	defer wb.mu.Unlock()
@@ -138,4 +140,11 @@ func logRecordKeyWithSeq(key []byte, seqNo uint64) []byte {
 	copy(encKey[n:], key)
 
 	return encKey
+}
+
+// 解析 LogRecord 的 key, 获取实际的 key 和事务序列号
+func parseLogRecordKey(key []byte) ([]byte, uint64) {
+	seqNo, n := binary.Uvarint(key)
+	realKey := key[n:]
+	return realKey, seqNo
 }
